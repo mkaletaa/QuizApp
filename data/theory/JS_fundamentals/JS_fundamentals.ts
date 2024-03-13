c188f08feadfc6c330f534a4ea28e69555700808
Mikołaj <kaleta222@gmail.com>
2024-03-13 21:11:13 +0100

    start loops

--- data/theory/JS_fundamentals/JS_fundamentals.ts
+++ data/theory/JS_fundamentals/JS_fundamentals.ts
@@ -2,7 +2,8 @@ import Hello_World from './Hello_World.json'
 import comments from './comments.json'
 import variables  from './variables.json'
 import conditionals from './conditionals.json'
+import loops from './loops.json'
 
 export const JS_fundamentals = {
-  Hello_World, comments, variables, conditionals
+  Hello_World, comments, variables, conditionals, loops
 }
