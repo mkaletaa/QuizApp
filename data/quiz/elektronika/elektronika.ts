7cb1f0acc7f5c6350adf1f6fefb7ba312dca6aca
Mikołaj <kaleta222@gmail.com>
2024-04-20 12:48:13 +0200

    data(555): add quiz, two images in theory and fix typos

--- data/quiz/elektronika/elektronika.ts
+++ data/quiz/elektronika/elektronika.ts
@@ -1,3 +1,4 @@
 import wzmacniacz_operacyjny from './wzmacniacz_operacyjny.json'
+import _555 from './_555.json'
 
-export const elektronika = { wzmacniacz_operacyjny }
+export const elektronika = { wzmacniacz_operacyjny, _555 }
