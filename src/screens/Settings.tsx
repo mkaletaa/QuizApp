fbb3645570576d8c83ddfb905c64e1d7103748de
Mikołaj <kaleta222@gmail.com>
2024-04-27 08:12:43 +0200

    ui(settings): set the ui

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -3,7 +3,8 @@ import { StyleSheet, Switch, View, Text, Pressable } from 'react-native'
 import { getValue, setValue } from '../utils/utilStorage'
 import { useNavigation } from '@react-navigation/native'
 import { randomOrder, contact, aboutTheApp } from '../../data/texts'
-
+import { Entypo } from '@expo/vector-icons'
+import { AntDesign } from '@expo/vector-icons'
 const StickyHeaderScrollView = () => {
   const [isShuffleSwitchEnabled, setIsShuffleSwitchEnabled] =
     useState<boolean>()
@@ -13,7 +14,8 @@ const StickyHeaderScrollView = () => {
       const shouldShuffle = await getValue('shuffle')
       if (shouldShuffle === null) setIsShuffleSwitchEnabled(false)
       else setIsShuffleSwitchEnabled(shouldShuffle)
-    } checkShuffle()
+    }
+    checkShuffle()
   }, [])
 
   function setShuffleStorage() {
@@ -23,14 +25,13 @@ const StickyHeaderScrollView = () => {
   //tutaj niepotrzebnie się wywołuje podczas każdego mountingu
   useEffect(() => {
     try {
-      setValue("shuffle", isShuffleSwitchEnabled)
+      setValue('shuffle', isShuffleSwitchEnabled)
     } catch (e) {
       console.error(e)
     }
   }, [isShuffleSwitchEnabled])
 
   const navigation = useNavigation()
-
 
   return (
     <View style={styles.container}>
@@ -39,19 +40,35 @@ const StickyHeaderScrollView = () => {
         style={{
           flexDirection: 'row',
           alignItems: 'center',
-          justifyContent: 'flex-start',
-          gap: 15, //used to be -15
-          marginBottom: 10,
-          paddingHorizontal: 15,
+          justifyContent: 'center',
+          // gap: 5, //used to be -15
+          // marginBottom: 10,
+          // paddingHorizontal: 15,
           // fontSize: 20
           // backgroundColor: 'blue',
+          borderBottomColor: 'silver',
+          borderBottomWidth: 1,
         }}
       >
+        <Text
+          style={{
+            width: '85%',
+            // backgroundColor: 'red',
+            fontSize: 16,
+            paddingRight: 15,
+            flex:1
+          }}
+        >
+          {randomOrder}
+        </Text>
         <Switch
+          style={{
+            // backgroundColor: 'yellow',
+            // width: '15%',
+          }}
           onValueChange={() => setShuffleStorage()}
           value={isShuffleSwitchEnabled}
         />
-        <Text>{randomOrder}</Text>
       </Pressable>
 
       <Pressable
@@ -60,20 +77,56 @@ const StickyHeaderScrollView = () => {
         style={{
           flexDirection: 'row',
           alignItems: 'center',
-          justifyContent: 'flex-start',
-          gap: 15, //used to be -15
+          justifyContent: 'space-between',
+          // flexGrow: 'center',
+          // gap: 15, //used to be -15
           marginBottom: 10,
-          paddingHorizontal: 15,
-          elevation: 5,
+          //paddingHorizontal: 15,
+          // elevation: 5,
           height: 50,
           // fontSize: 20
-          backgroundColor: 'blue',
+          // backgroundColor: 'blue',
+          borderBottomColor: 'silver',
+          borderBottomWidth: 1,
+          // flexBasis: 'baseline',
         }}
       >
-        <Text>{aboutTheApp}</Text>
+        <Entypo
+          name="info-with-circle"
+          size={24}
+          color="steelblue"
+          style={{
+            // width: '10%',
+
+            // backgroundColor: 'silver',
+          }}
+        />
+        <Text style={{ fontSize: 18,
+        //  backgroundColor: 'red', 
+        // width: "auto" ,
+        flex:1,
+        paddingLeft: 10
+        // alignSelf:"stretch"
+        }}>
+          {aboutTheApp}
+        </Text>
+        <AntDesign
+          name="right"
+          size={24}
+          color="dimgrey"
+          style={{
+            // width: '10%',
+            // backgroundColor: 'white',
+          }}
+        />
       </Pressable>
 
-      <Text>
+      <Text
+        style={{
+          opacity: 0.6,
+          marginTop: 10,
+        }}
+      >
         {contact}: <Text>learn.everything.app@proton.me</Text>
       </Text>
     </View>
@@ -83,6 +136,8 @@ const StickyHeaderScrollView = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingHorizontal: 15,
+    paddingTop: 15,
   },
 })
 
