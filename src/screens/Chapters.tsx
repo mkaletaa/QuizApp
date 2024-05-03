e492bd782d2cf9d47f98d2ba787de9420c07060f
Mikołaj <kaleta222@gmail.com>
2024-05-03 11:12:37 +0200

    ui(Chapters): add skia mesh gradient

--- src/screens/Chapters.tsx
+++ src/screens/Chapters.tsx
@@ -7,6 +7,7 @@ import Card from '../components/Card'
 import utilStyles from '../utils/styles'
 import RandomQuestion from '../components/ui/RandomQuestion'
 import { LinearGradient } from 'expo-linear-gradient'
+import { Canvas, Patch, vec, Surface, Oval, Blur } from '@shopify/react-native-skia'
 
 export default function Chapters() {
   const navigation = useNavigation()
@@ -16,26 +17,62 @@ export default function Chapters() {
     navigation.navigate('Topics', { chapterName: chapName })
   }
 
+  const colors = ['#61dafb', '#fb61da', '#61fbcf', '#dafb61']
+  const width = 360
+  const height = 600
+  const topLeft = { pos: vec(0, 0), c1: vec(0, 0), c2: vec(width, 0) }
+  const topRight = {
+    pos: vec(width, 0),
+    c1: vec(width, height),
+    c2: vec(width, 0),
+  }
+  const bottomRight = {
+    pos: vec(width, height),
+    c1: vec(width, height),
+    c2: vec( width, height),
+  }
+  const bottomLeft = {
+    pos: vec(0, height),
+    c1: vec(0, height),
+    c2: vec(width, height),
+  }
+
   return (
     <View
       style={{
         alignItems: 'center',
       }}
     >
-      <LinearGradient
+      {/* <LinearGradient
         // Button Linear Gradient
-        colors={['#EC9F05', '#FF4E00']}
-        start={{ x: 0, y: .6 }}
+        colors={['rgba(238, 130, 238, .2)', 'transparent']}
+        start={{ x: 0, y: 0.6 }}
         end={{ x: 1, y: 0 }}
         style={{
           width: '100%',
           height: '100%', //has to be 100% cuz if Math component, Tile cannot be pressed
           position: 'absolute',
           bottom: 0,
-          // opacity: .8
-          // transform: [{ rotate: '25deg' }], // Obrót gradientu o 45 stopni
         }}
-      ></LinearGradient>
+      ></LinearGradient> */}
+
+      <Canvas
+        style={{
+          width: 360,
+          height: 600,
+          position: 'absolute',
+          bottom: 0
+        }}
+      >
+        <Oval x={-100} y={450} width={396} height={228} color="purple" opacity={.07} />
+        <Oval x={300} y={150} width={496} height={228} color="yellow" opacity={.09} />
+        {/* <Patch
+          colors={colors}
+          patch={[topLeft, topRight, bottomRight, bottomLeft]}
+        /> */}
+        <Blur blur={34} />
+      </Canvas>
+
       <RandomQuestion chapName={'__All__'} />
       <ScrollView contentContainerStyle={utilStyles.scrollViewCardContainer}>
         <StatusBar style="auto" />
