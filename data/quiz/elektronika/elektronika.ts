4b8340ea6087fc29c07e0e25ab2cea1b5348547d
Mikołaj <kaleta222@gmail.com>
2024-05-04 13:45:00 +0200

    data(układy_logiczne): create a quiz and update theory

--- data/quiz/elektronika/elektronika.ts
+++ data/quiz/elektronika/elektronika.ts
@@ -1,4 +1,5 @@
 import wzmacniacz_operacyjny from './wzmacniacz_operacyjny.json'
 import _555 from './_555.json'
+import układy_logiczne from './układy_logiczne.json'
 
-export const elektronika = { wzmacniacz_operacyjny, _555 }
+export const elektronika = { wzmacniacz_operacyjny, _555, układy_logiczne }
