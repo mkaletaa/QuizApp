14c4d75662704e32914e165550699bfd47774f17
Mikołaj <kaleta222@gmail.com>
2024-05-05 10:33:56 +0200

    config: remove react-native-skia and delete code related to it

--- src/screens/Chapters.tsx
+++ src/screens/Chapters.tsx
@@ -7,7 +7,7 @@ import Card from '../components/Card'
 import utilStyles from '../utils/styles'
 import RandomQuestionButton from '../components/molecules/atoms/RandomQuestionButton'
 import { LinearGradient } from 'expo-linear-gradient'
-import { Canvas, Patch, vec, Oval, Blur } from '@shopify/react-native-skia'
+// import { Canvas, Patch, vec, Oval, Blur } from '@shopify/react-native-skia'
 import { screenBackground } from '../utils/constants'
 import Gradient from '../components/molecules/Gradient'
 
@@ -22,22 +22,22 @@ export default function Chapters() {
   const colors = ['#61dafb', '#fb61da', '#61fbcf', '#dafb61']
   const width = 360
   const height = 600
-  const topLeft = { pos: vec(0, 0), c1: vec(0, 0), c2: vec(width, 0) }
-  const topRight = {
-    pos: vec(width, 0),
-    c1: vec(width, height),
-    c2: vec(width, 0),
-  }
-  const bottomRight = {
-    pos: vec(width, height),
-    c1: vec(width, height),
-    c2: vec(width, height),
-  }
-  const bottomLeft = {
-    pos: vec(0, height),
-    c1: vec(0, height),
-    c2: vec(width, height),
-  }
+  // const topLeft = { pos: vec(0, 0), c1: vec(0, 0), c2: vec(width, 0) }
+  // const topRight = {
+  //   pos: vec(width, 0),
+  //   c1: vec(width, height),
+  //   c2: vec(width, 0),
+  // }
+  // const bottomRight = {
+  //   pos: vec(width, height),
+  //   c1: vec(width, height),
+  //   c2: vec(width, height),
+  // }
+  // const bottomLeft = {
+  //   pos: vec(0, height),
+  //   c1: vec(0, height),
+  //   c2: vec(width, height),
+  // }
 
   return (
     <View
