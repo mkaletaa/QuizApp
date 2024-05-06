b42d835e0674e955f8efc4d3e32203e6509e2f74
Mikołaj <kaleta222@gmail.com>
2024-05-06 16:08:08 +0200

    data: add missing folders for cat_1 and cat_2

--- data/quiz/quizModule.ts
+++ data/quiz/quizModule.ts
@@ -1,5 +1,5 @@
-import { cat_1 } from "./cat_1";
-import { cat_2 } from "./cat_2";
-import { general_information } from "./general_information/general_information";
+import { cat_1 } from './cat_1/cat_1'
+import { cat_2 } from './cat_2/cat_2'
+import { general_information } from './general_information/general_information'
 
 export const quiz = { cat_1, cat_2, general_information }
