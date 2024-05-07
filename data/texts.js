4fb45d28172aa9f3074ab70a4f5effbf98b1c4ba
Mikołaj <kaleta222@gmail.com>
2024-05-07 21:20:32 +0200

    chore(texts): remove longPress text from all places

--- data/texts.js
+++ data/texts.js
@@ -27,8 +27,6 @@ export const mailSubject = ' - błąd'
 export const mailBody =
   '\n\nNawet najlepsi popełniają błędy. Opisz co jest nie tak, a ja to naprawię. \n(możesz usunąć ciało tego maila z wyjątkiem pierwszej linii)'
 export const noQuestions = 'Brak pytań do wyświetlenia'
-export const longPress =
-  'Przytrzymaj palec na ekranie, by zobaczyć jeszcze raz pytanie'
 export const randomOrder = "losowa kolejność pytań"
 export const contact = 'Kontakt'
 export const aboutTheApp = 'O aplikacji'
