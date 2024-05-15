8303d9d95e89d7419e671eeeecb2d239a6f2ce43
Mikołaj <kaleta222@gmail.com>
2024-05-15 18:22:31 +0200

    data(konwertery_kodów): add quiz

--- data/quiz/elektronika/elektronika.ts
+++ data/quiz/elektronika/elektronika.ts
@@ -1,5 +1,11 @@
 import wzmacniacz_operacyjny from './wzmacniacz_operacyjny.json'
 import _555 from './_555.json'
 import układy_logiczne from './układy_logiczne.json'
+import konwertery_kodów from './konwertery_kodów.json'
 
-export const elektronika = { wzmacniacz_operacyjny, _555, układy_logiczne }
+export const elektronika = {
+  wzmacniacz_operacyjny,
+  _555,
+  układy_logiczne,
+  konwertery_kodów,
+}
