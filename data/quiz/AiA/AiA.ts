b42c7feb781e0d9837aec5b27e1d1ce491c56b07
Mikołaj <kaleta222@gmail.com>
2024-06-25 11:15:04 +0200

    data(AiA): add całki quiz

--- data/quiz/AiA/AiA.ts
+++ data/quiz/AiA/AiA.ts
@@ -1,4 +1,5 @@
 import pochodne from './pochodne.json'
+import całki from './całki.json'
 import egzamin_teoria from './egzamin_teoria.json'
 
-export const AiA = { egzamin_teoria, pochodne }
+export const AiA = { egzamin_teoria, pochodne, całki }
