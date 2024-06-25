5a489a37e974ae5a680f389bfe88fb3540ebfa58
Mikołaj <kaleta222@gmail.com>
2024-06-25 21:16:35 +0200

    data(AiA): enhance analiza-pozostałe quiz

--- data/data.ts
+++ data/data.ts
@@ -6,16 +6,17 @@ export const chapters = [
   },
   {
     name: 'AiA',
-    image: 'https://i.postimg.cc/zvkLpPFP/cpp.png',
+    image: 'https://i.postimg.cc/Df9Ht5kQ/image.png',
+    des: 'Znajduje się tu tylko analiza, bo algebry mi się już nie chciało dodawać xD. Ale jak coś to okrutnie polecam <a href="https://www.youtube.com/watch?v=qoaaCboDWR8&list=PLl2G2wgLAhWA5pRMSfPd9ISEaqOGQoHtk">tę playlistę</a> o macierzach. Gość tłumaczy lepiej niż Piękosz.',
   },
   {
     name: 'elektronika',
     image: 'https://i.postimg.cc/y8yYNN95/zaba.jpg',
-    des: 'Jeśli myślisz, że będzie źle, mylisz się. Będzie gorzej. Ten przedmiot to jakiś ewenement, bo kolokwia są fajniejsze niż ćwiczenia. Nie dość, że wychodzisz wcześniej, to jeszcze nie musisz słuchać darcia mordy. Ale nie bój żaby, dzięki mnie zdasz śpiewająco na 3. <br></br> Większość informacji z tego rozdziału wziąłem <a href="https://mega.nz/folder/MN0FRDqT#E07ZRcEyDT8d-hi58KpdDg/folder/lcdE3DDS">stąd</a> i <a href="https://wieik.ovh/dokuph_8.html">stąd</a>. Nie są moje. Zdarzają się w nich błędy, więc jak coś to lepiej uczyć się z aplikacji niż z nich. No i zwracaj największą uwagę na schematy.',
+    des: 'Jeśli myślisz, że będzie źle, mylisz się. Będzie gorzej. Ten przedmiot to jakiś ewenement. Kolokwia są fajniejsze niż ćwiczenia, bo nie dość, że wychodzisz wcześniej, to jeszcze nie musisz słuchać darcia mordy. Ale nie bój żaby, dzięki mnie zdasz śpiewająco na 3. <br></br> Większość informacji z tego rozdziału wziąłem <a href="https://mega.nz/folder/MN0FRDqT#E07ZRcEyDT8d-hi58KpdDg/folder/lcdE3DDS">stąd</a> i <a href="https://wieik.ovh/dokuph_8.html">stąd</a>. Nie są moje. Zdarzają się w nich błędy, więc jak coś to lepiej uczyć się z aplikacji niż z nich. No i zwracaj największą uwagę na schematy.',
   },
   {
     name: 'elektrotechnika',
-    image: 'https://i.postimg.cc/Xv0Xm9DV/chrab-szcz.png',
+    image: 'https://i.postimg.cc/B6XZwN99/image.png',
     des: 'Jeśli chodzi o laboratoria, to z Prusakiem zdasz z palcem wiadomo gdzie, a z Chrabąszczem choćbyś przepisał słowo w słowo jego notatki to i tak nie dostaniesz 5 (nie żartuję). Gość potrafi uwalić cię za jedno źle użyte słowo (tutaj też nie żartuję). Na szczęście we wszystkich grupach i na wszystkich termianch pytania były te same. <br></br>Jeśli chodzi o ćwiczenia, to na początku pyta kilka osób, ale bez spiny, to nie żabulec. Kolokwia z ćwiczeń już nie sprawdza tak surowo. <br></br>Tak, na okładce to Chrabąszcz',
   },
   // {
@@ -40,29 +41,29 @@ export const topics = {
   AiA: [
     {
       name: 'egzamin_teoria',
-      image: 'https://i.postimg.cc/zvkLpPFP/cpp.png',
+      image: 'https://i.postimg.cc/nVDqwG1t/image.png',
     },
     {
       name: 'granice',
-      image: 'https://i.postimg.cc/zvkLpPFP/cpp.png',
+      image: 'https://i.postimg.cc/3NYr1hBN/image.png',
     },
     {
       name: 'pochodne',
-      image: 'https://i.postimg.cc/zvkLp.png',
+      image: 'https://i.postimg.cc/yN4McPnY/image.png',
     },
     {
       name: 'całki',
-      image: 'https://i.postimg.cc/zvkLp.png',
+      image: 'https://i.postimg.cc/c1rF13MD/image.png',
     },
     {
       name: 'analiza_pozostałe',
-      image: 'https://i.postimg.cc/'
+      image: 'https://i.postimg.cc/TwhJw8my/image.png',
     },
   ],
   elektronika: [
     {
       name: 'wzmacniacz_tranzystorowy',
-      image: 'https://i.postimg.cc/jdYvFSMz/image.png',
+      image: 'https://i.postimg.cc/fyPzNdst/image.png',
     },
     {
       name: 'wzmacniacz_operacyjny',
@@ -105,6 +106,10 @@ export const topics = {
     {
       name: 'RLC',
       image: 'https://i.postimg.cc/9Q8N13XP/rlc.png',
+    },
+    {
+      name: 'egzamin',
+      image: 'https://i.postimg.cc/tTMQZHM8/image.png',
     },
   ],
   // metody_programowania: [
