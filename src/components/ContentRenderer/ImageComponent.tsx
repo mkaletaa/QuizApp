0bf4a3946421682462532959bd272bd952edc408
Mikołaj <kaleta222@gmail.com>
2024-07-03 14:19:37 +0200

    ui(ImageComponent): adjust the size of an Image

--- src/components/ContentRenderer/ImageComponent.tsx
+++ src/components/ContentRenderer/ImageComponent.tsx
@@ -1,15 +1,15 @@
 import { AntDesign } from '@expo/vector-icons'
-import { Image } from 'expo-image'
+import { Image as ExpoImage } from 'expo-image'
 import { LinearGradient } from 'expo-linear-gradient'
 import React, { useEffect, useState } from 'react'
 import {
   Dimensions,
-  // Image,
+  Image,
   Modal,
   StyleSheet,
   Text,
   TouchableOpacity,
-  View
+  View,
 } from 'react-native'
 import ImageViewer from 'react-native-image-zoom-viewer'
 import useStore from '../../utils/store'
@@ -23,96 +23,74 @@ const ImageComponent = ({
   const [modalVisible, setModalVisible] = useState(false)
   const [indexState, setIndexState] = useState(0)
   const images = useStore(state => state.images)
-  // const [isLoading, setIsLoading] = useState(true)
+  const [imageSize, setImageSize] = useState({ width: 0, height: 0 })
+  // const [loading, setLoading] = useState(true)
+
+  const ratio = 0.9 // ratio between width of the image to screen (or container) width
 
   useEffect(() => {
-    // console.log('images: ', useStore.getState().images.length)\
-
-    // karuzela odpala się tylko gdy zdjęcie jest w Theory
     if (useStore.getState().carousel) {
-      useStore.getState().addImage(value, description) //dodaj image do zustand
-      setIndexState(useStore.getState().images.length - 1) //ustaw indeks na podstawie tego ile zdjęć znajduje się w zustand w momencie mountingu
+      useStore.getState().addImage(value, description)
+      setIndexState(useStore.getState().images.length - 1)
     }
   }, [])
+
+  useEffect(() => {
+    Image.getSize(
+      value,
+      (width, height) => {
+        const scaledWidth = containerWidth * ratio
+        const scaledHeight = (height / width) * scaledWidth
+        setImageSize({ width: scaledWidth, height: scaledHeight })
+        // setLoading(false)
+      },
+      error => {
+        console.error(`Failed to get image size: ${error.message}`)
+        // setLoading(false)
+      }
+    )
+  }, [value, containerWidth, ratio])
 
   const [descriptionState, setDescriptionState] = useState<string | undefined>(
     undefined
   )
 
   useEffect(() => {
-    // console.log('images', images)
     setDescriptionState(description || images[indexState]?.des)
   }, [modalVisible, indexState])
+
   const setShowPopup = useStore(state => state.setShowPopup)
 
   const openModal = () => {
     setModalVisible(true)
-        setShowPopup(false)
-
+    setShowPopup(false)
   }
 
   const closeModal = () => {
     setModalVisible(false)
   }
 
-  function setHeight(
-    orientation:
-      | 'vertical'
-      | 'horizontal'
-      | 'square'
-      | null
-      | 'portrait'
-      | 'landscape'
-  ): number {
-    // console.log('🚀 ~ orientation:', orientation)
-    switch (orientation) {
-      case 'portrait':
-      case 'vertical':
-        return containerWidth * ratio * 1.3 //16/9 czy jakoś tak
-      case 'horizontal':
-      case 'landscape':
-        return containerWidth * ratio * 0.6
-      case 'square':
-        containerWidth * ratio
-      default:
-        return containerWidth * ratio
-    }
-  }
-
-  const ratio = 0.9 //ratio between width of the image to screen (or container) width
-
   return (
     <React.Fragment>
       <TouchableOpacity onPress={openModal} activeOpacity={0.7}>
-        {/* <View style={{}}> */}
-        {/* <ActivityIndicator size="large" color="#0000ff" /> */}
-        <View style={{ backgroundColor: 'lightgray', borderRadius: 8 }}>
-          <Image
+        <View style={[styles.imageContainer]}>
+          <ExpoImage
             key={value}
             style={[
               styles.image,
-              { width: containerWidth * ratio, height: setHeight(orientation) },
+              { width: imageSize.width, height: imageSize.height },
             ]}
             source={{
               uri: value,
             }}
-            // placeholder={
-            //   '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj['
-            // }
             placeholder={require('../../../assets/failImage.png')}
-            //todo: spróbowac require placeholdera i ogarnąć border w View, onError
             contentFit="cover"
-            // placeholderContentFit="cover"
           />
         </View>
-
-        {/* </View> */}
       </TouchableOpacity>
       {description && (
         <Text style={{ opacity: 0.5, marginTop: -10 }}>{description}</Text>
       )}
-      {/* <StatusBar backgroundColor="rgba(255, 0, 0, 1)" hidden translucent /> */}
-
       <Modal
         visible={modalVisible}
         animationType="fade"
@@ -129,11 +107,9 @@ const ImageComponent = ({
             style={styles.closeButton}
           />
           <ImageViewer
-            //jeśli w zustandzie są jakieś zdjęcia to ich użyj, a jak nie to użyj propsa
             imageUrls={images.length !== 0 ? images : [{ url: value }]}
             onChange={i =>
               setDescriptionState(
-                //to samo co wyżej
                 images.length !== 0 ? images[i]?.des : description
               )
             }
@@ -147,9 +123,6 @@ const ImageComponent = ({
             loadingRender={() => {
               return <Text>...</Text>
             }}
-            // menus={e => null}
-
-            // onLongPress={null}
           />
           {descriptionState && (
             <View
@@ -161,19 +134,7 @@ const ImageComponent = ({
               }}
             >
               <LinearGradient
-                colors={[
-                  'rgba(0, 0, 0, 0)',
-                  // 'rgba(0, 0, 0, .05)',
-                  // 'rgba(0, 0, 0, .10)',
-                  // 'rgba(0, 0, 0, .15)',
-                  // 'rgba(0, 0, 0, .20)',
-                  // 'rgba(0, 0, 0, .25)',
-                  // 'rgba(0, 0, 0, .30)',
-                  // 'rgba(0, 0, 0, .35)',
-                  // 'rgba(0, 0, 0, .40)',
-                  // 'rgba(0, 0, 0, .45)',
-                  'rgba(0, 0, 0, .5)',
-                ]}
+                colors={['rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, .5)']}
                 style={{ width: '100%', height: 20 }}
               ></LinearGradient>
               <Text
@@ -197,11 +158,16 @@ const ImageComponent = ({
 }
 
 const styles = StyleSheet.create({
-  image: {
+  imageContainer: {
+    backgroundColor: 'lightgray',
+    borderRadius: 8,
     borderWidth: 1,
     borderColor: 'rgba(0, 0, 0, .1)',
-    borderRadius: 8,
-    // flex: 1
+    overflow: 'hidden',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
   },
   modalContainer: {
     height: Dimensions.get('window').height,
@@ -220,8 +186,8 @@ const styles = StyleSheet.create({
     left: 20,
     padding: 5,
     zIndex: 1,
-    textShadowColor: 'rgba(0, 0, 0, .9)', // kolor cienia
-    textShadowRadius: 7, // promień cienia
+    textShadowColor: 'rgba(0, 0, 0, .9)',
+    textShadowRadius: 7,
   },
 })
 
