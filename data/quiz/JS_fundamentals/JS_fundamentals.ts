db136380307ccaa3274b7931746c588605074585
Mikołaj <kaleta222@gmail.com>
2024-07-27 18:41:41 +0200

    data(JS_fundamentals): edit variables theory

--- data/quiz/JS_fundamentals/JS_fundamentals.ts
+++ data/quiz/JS_fundamentals/JS_fundamentals.ts
@@ -3,12 +3,3 @@ import comments from './comments.json'
 export const JS_fundamentals = {
   comments,
 }
-//** A **//
-
-/*\n
-\t//B\n
-*/
-
-// D // D
-
-/* * ** E */
--- data/theory/general_information/general_information.ts
+++ data/theory/general_information/general_information.ts
@@ -2,4 +2,9 @@ import historical_background from './historical_background.json'
 import about_JavaScript from './about_JavaScript.json'
 import test from './test1.json'
 
-export const general_information = { historical_background, about_JavaScript, test }
+export const general_information = {
+  historical_background,
+  about_JavaScript,
+  test,
+}
+
