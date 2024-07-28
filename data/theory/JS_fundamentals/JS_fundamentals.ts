f419342ce55d1eed5cf0a81a408f29ac2a4773b9
Mikołaj <kaleta222@gmail.com>
2024-07-28 11:47:49 +0200

    data(JS_fundamentals): add operators theory

--- data/theory/JS_fundamentals/JS_fundamentals.ts
+++ data/theory/JS_fundamentals/JS_fundamentals.ts
@@ -3,8 +3,14 @@ import comments from './comments.json'
 import variables  from './variables.json'
 import conditionals from './conditionals.json'
 import loops from './loops.json'
+import operators from './operators.json'
 
 export const JS_fundamentals = {
-  Hello_World, comments, variables, conditionals, loops
+  Hello_World,
+  comments,
+  variables,
+  conditionals,
+  loops,
+  operators,
 }
 
