404f6a322bfeac6364a32ffeef089daa267af9a5
Mikołaj <kaleta222@gmail.com>
2024-07-28 10:04:16 +0200

    data(JS_fundamentals): add conditionals quiz

--- data/quiz/JS_fundamentals/JS_fundamentals.ts
+++ data/quiz/JS_fundamentals/JS_fundamentals.ts
@@ -1,8 +1,10 @@
 import comments from './comments.json'
 import variables from './variables.json'
+import conditionals from './conditionals.json'
 
 export const JS_fundamentals = {
   comments,
   variables,
+  conditionals,
 }
-
+//, tylko jeden break w switchu
