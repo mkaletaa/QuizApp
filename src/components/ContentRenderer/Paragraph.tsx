74239963f7f5b7bc31da277a81eb8188cc9b4ab8
Mikołaj <kaleta222@gmail.com>
2024-08-04 22:13:51 +0200

    get the attributes of an html tag

--- src/components/ContentRenderer/Paragraph.tsx
+++ src/components/ContentRenderer/Paragraph.tsx
@@ -1,31 +1,73 @@
 import React from 'react'
 import RenderHtml from 'react-native-render-html'
 import { textColor } from '../../utils/constants'
+import { Alert } from 'react-native'
 
+export default function Paragraph({ value, width, props }) {
+  let modifiedValue =
+    `<span id="customSpan" style="font-size: 18px; color: ${textColor}; line-height: 25px; width: ${
+      props?.toLeft ? width * 0.9 : 'auto'
+    }px">` +
+    '<ins style="color: purple" data-role="admin" id="customId">Press me!</ins> ' +
+    value +
+    '</span>'
 
-export default function Paragraph({value, width, props}) {
-      let modifiedValue =
-        `<span style=" font-size: 18px; color: ${textColor}; line-height: 25px; width: ${
-          props?.toLeft ? width * 0.9 : null
-        }px">` +
-        '<span>Press me!</span>' +
-        value +
-        '</span>'
-
-      function H1Renderer({ TDefaultRenderer, ...props }) {
-        const onPress = () => console.log('pressed!')
-        return <TDefaultRenderer {...props} onPress={onPress} />
+  function getObjectKeys(obj, seen = new Set()) {
+    // Iteracja przez klucze obiektu
+    for (const key in obj) {
+      // Sprawdzanie, czy obiekt ma dany klucz (pomijanie dziedziczonych)
+      if (obj.hasOwnProperty(key)) {
+        // Sprawdzanie, czy wartość klucza nie jest `undefined`
+        if (obj[key] !== undefined) {
+          console.log(key) // Wypisanie klucza
+        }
       }
+    }
+  }
 
-      const renderers = {
-        span: H1Renderer,
-      }
+  function InsRenderer({ TDefaultRenderer, ...props }) {
+    const {
+      textProps, //{"selectable":false,"allowFontScaling":true}
+      tagName, //undefined
+      children, //undefined
+      node,
+      propsFromParent, //{"collapsedMarginTop":null}
+      attribs, //undefined
+      type, //text
+      tnode,
+      sharedProps, //*
+      TNodeChildrenRenderer,
+      style, //{"fontSize":18,"color":"purple","lineHeight":25,"textDecorationLine":"underline","textDecorationStyle":"solid"}
+      propsForChildren, //{}
+      InternalRenderer, //undefined ?
+      renderIndex, //0
+      renderLength, //3
+      viewProps, //{}
+    } = props
+    // const id = domNode
 
-      return (
-        <RenderHtml
-          contentWidth={width}
-          source={{ html: modifiedValue }}
-          renderers={renderers}
-        />
-      )
+    const onPress = () => {
+      console.log(`Props: ${JSON.stringify(tnode.init.domNode.attribs['id'])}`)
+    }
+
+    return (
+      <TDefaultRenderer
+        {...props}
+        onPress={onPress}
+        style={{ color: 'blue', textDecorationLine: 'underline' }} // Optional: Styling for interactivity
+      />
+    )
+  }
+
+  const renderers = {
+    ins: InsRenderer,
+  }
+
+  return (
+    <RenderHtml
+      contentWidth={width}
+      source={{ html: modifiedValue }}
+      renderers={renderers}
+    />
+  )
 }
