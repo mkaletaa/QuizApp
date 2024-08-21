81f87585aae4126fa1035266b1a777009feaa249
Mikołaj <kaleta222@gmail.com>
2024-08-21 20:25:21 +0200

    data(data_structures): add arrays quiz (not finished)

--- data/quiz/data_structures/data_structures.ts
+++ data/quiz/data_structures/data_structures.ts
@@ -3,11 +3,13 @@ import numbers from './numbers.json'
 import objects_fundamentals from './objects_fundamentals.json'
 import strings from './strings.json'
 import type_conversion from './type_conversion.json'
+import arrays from './arrays.json'
 
 export const data_structures = {
   numbers,
   introduction,
   strings,
   type_conversion,
+  arrays,
   objects_fundamentals,
 }
