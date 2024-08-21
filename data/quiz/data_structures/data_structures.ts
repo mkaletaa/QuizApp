f4c6576b9026b0a957a1eb873559d2bb5c879ec3
Mikołaj <kaleta222@gmail.com>
2024-08-21 15:35:45 +0200

    data(data_structures): add strings quiz

--- data/quiz/data_structures/data_structures.ts
+++ data/quiz/data_structures/data_structures.ts
@@ -1,8 +1,10 @@
 import introduction from './introduction.json';
 import numbers from './numbers.json';
+import strings from './strings.json';
 
 
 export const data_structures = {
   numbers,
   introduction,
+  strings
 }
