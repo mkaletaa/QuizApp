f04cfc8820fa2410f4a27c733f174a03cd7aa662
Mikołaj <kaleta222@gmail.com>
2024-09-07 18:49:07 +0200

    data(asynchronicity): finish promises and timeouts quizes and edit theoi Theories

---
+++ data/quiz/asynchronicity/asynchronicity.ts
@@ -0,0 +1,9 @@
+import async_and_await from './async_and_await.json'
+import promises from './promises.json'
+import timeouts_and_intervals from './timeouts_and_intervals.json'
+
+export const asynchronicity = {
+  timeouts_and_intervals,
+  promises,
+  async_and_await,
+}
--- data/quiz/quizModule.ts
+++ data/quiz/quizModule.ts
@@ -1,9 +1,11 @@
 import { about_JavaScript } from './about_JavaScript/about_JavaScript'
 import { JS_fundamentals } from './JS_fundamentals/JS_fundamentals'
 import { data_structures } from './data_structures/data_structures'
+import { asynchronicity } from './asynchronicity/asynchronicity'
 
 export const quiz = {
   about_JavaScript,
   JS_fundamentals,
-  data_structures
+  data_structures,
+  asynchronicity
 }
