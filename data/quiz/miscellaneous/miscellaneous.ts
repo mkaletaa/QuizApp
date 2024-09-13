a38da573f6739c953a984168e8c9f30c9b196506
Mikołaj <kaleta222@gmail.com>
2024-09-13 11:09:25 +0200

    data(TypeScript): edit generics a bit

--- data/quiz/miscellaneous/miscellaneous.ts
+++ data/quiz/miscellaneous/miscellaneous.ts
@@ -1,6 +1,7 @@
-import destructuring from './destructuring.json'
-import execution_context from './execution_context.json'
-import spread_and_rest from './spread_and_rest.json'
+import destructuring from './destructuring.json';
+import execution_context from './execution_context.json';
+import spread_and_rest from './spread_and_rest.json';
+
 
 export const miscellaneous = {
   execution_context,
@@ -8,4 +9,11 @@ export const miscellaneous = {
   destructuring,
 }
 
+type ConditionalType<T> = T extends string ? string : number;
 
+function f<T>(entity: T): ConditionalType<T> {
+  if (typeof entity === 'string') {
+    return entity as ConditionalType<T>;
+  }
+  return 42 as ConditionalType<T>;
+}
