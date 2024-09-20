87e7156b203647d06a7f0a871c5e4165cf7a332d
Mikołaj <kaleta222@gmail.com>
2024-09-20 10:38:39 +0200

    feat(CodeSettings): add more themes

--- src/components/CodeSettings.tsx
+++ src/components/CodeSettings.tsx
@@ -1,11 +1,14 @@
-import React, { useEffect } from 'react'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-import { selectCodeTheme } from '../../data/texts'
-import { Colors } from '../utils/constants'
-import useStore from '../utils/store'
-import { setValue } from '../utils/utilStorage'
-import ContentRenderer from './ContentRenderer/_ContentRenderer'
+
+
+import { selectCodeTheme } from '../../data/texts';
+import { Colors } from '../utils/constants';
+import useStore from '../utils/store';
+import { setValue } from '../utils/utilStorage';
+import ContentRenderer from './ContentRenderer/_ContentRenderer';
+
 
 export default function CodeSettings() {
   const setHljsStyle = useStore(state => state.setHljsStyle)
@@ -16,16 +19,104 @@ export default function CodeSettings() {
   }, [hljsStyle])
 
   const stylesArray = [
-    { label: 'Night Owl', value: 'nightOwl' },
+    { label: 'A11y Dark', value: 'a11yDark' },
+    { label: 'A11y Light', value: 'a11yLight' },
+    { label: 'Agate', value: 'agate' },
+    { label: 'An Old Hope', value: 'anOldHope' },
+    { label: 'Android Studio', value: 'androidstudio' },
+    { label: 'Arduino Light', value: 'arduinoLight' },
+    { label: 'Arta', value: 'arta' },
     { label: 'Ascetic', value: 'ascetic' },
+    { label: 'Atelier Cave Dark', value: 'atelierCaveDark' },
+    { label: 'Atelier Cave Light', value: 'atelierCaveLight' },
+    { label: 'Atelier Dune Dark', value: 'atelierDuneDark' },
+    { label: 'Atelier Dune Light', value: 'atelierDuneLight' },
+    { label: 'Atelier Estuary Dark', value: 'atelierEstuaryDark' },
+    { label: 'Atelier Estuary Light', value: 'atelierEstuaryLight' },
+    { label: 'Atelier Forest Dark', value: 'atelierForestDark' },
+    { label: 'Atelier Forest Light', value: 'atelierForestLight' },
+    { label: 'Atelier Heath Dark', value: 'atelierHeathDark' },
+    { label: 'Atelier Heath Light', value: 'atelierHeathLight' },
+    { label: 'Atelier Lakeside Dark', value: 'atelierLakesideDark' },
+    { label: 'Atelier Lakeside Light', value: 'atelierLakesideLight' },
+    { label: 'Atelier Plateau Dark', value: 'atelierPlateauDark' },
+    { label: 'Atelier Plateau Light', value: 'atelierPlateauLight' },
+    { label: 'Atelier Savanna Dark', value: 'atelierSavannaDark' },
+    { label: 'Atelier Savanna Light', value: 'atelierSavannaLight' },
+    { label: 'Atelier Seaside Dark', value: 'atelierSeasideDark' },
+    { label: 'Atelier Seaside Light', value: 'atelierSeasideLight' },
+    { label: 'Atelier Sulphurpool Dark', value: 'atelierSulphurpoolDark' },
+    { label: 'Atelier Sulphurpool Light', value: 'atelierSulphurpoolLight' },
+    { label: 'Atom One Dark Reasonable', value: 'atomOneDarkReasonable' },
     { label: 'Atom One Dark', value: 'atomOneDark' },
     { label: 'Atom One Light', value: 'atomOneLight' },
+    { label: 'Brown Paper', value: 'brownPaper' },
+    { label: 'Codepen Embed', value: 'codepenEmbed' },
+    { label: 'Color Brewer', value: 'colorBrewer' },
+    { label: 'Darcula', value: 'darcula' },
+    { label: 'Dark', value: 'dark' },
+    { label: 'Default Style', value: 'defaultStyle' },
+    { label: 'Docco', value: 'docco' },
     { label: 'Dracula', value: 'dracula' },
+    { label: 'Far', value: 'far' },
+    { label: 'Foundation', value: 'foundation' },
+    { label: 'GitHub Gist', value: 'githubGist' },
+    { label: 'GitHub', value: 'github' },
+    { label: 'GML', value: 'gml' },
+    { label: 'Google Code', value: 'googlecode' },
+    { label: 'Gradient Dark', value: 'gradientDark' },
+    { label: 'Grayscale', value: 'grayscale' },
+    { label: 'Gruvbox Dark', value: 'gruvboxDark' },
+    { label: 'Gruvbox Light', value: 'gruvboxLight' },
+    { label: 'Hopscotch', value: 'hopscotch' },
+    { label: 'Hybrid', value: 'hybrid' },
+    { label: 'Idea', value: 'idea' },
+    { label: 'IR Black', value: 'irBlack' },
+    { label: 'ISBL Editor Dark', value: 'isblEditorDark' },
+    { label: 'ISBL Editor Light', value: 'isblEditorLight' },
+    { label: 'Kimbie Dark', value: 'kimbieDark' },
+    { label: 'Kimbie Light', value: 'kimbieLight' },
+    { label: 'Lightfair', value: 'lightfair' },
+    { label: 'Lioshi', value: 'lioshi' },
+    { label: 'Magula', value: 'magula' },
+    { label: 'Mono Blue', value: 'monoBlue' },
+    { label: 'Monokai Sublime', value: 'monokaiSublime' },
+    { label: 'Monokai', value: 'monokai' },
+    { label: 'Night Owl', value: 'nightOwl' },
+    { label: 'NNFX Dark', value: 'nnfxDark' },
+    { label: 'NNFX', value: 'nnfx' },
+    { label: 'Nord', value: 'nord' },
+    { label: 'Obsidian', value: 'obsidian' },
+    { label: 'Ocean', value: 'ocean' },
+    { label: 'Paraiso Dark', value: 'paraisoDark' },
+    { label: 'Paraiso Light', value: 'paraisoLight' },
+    { label: 'Pojoaque', value: 'pojoaque' },
+    { label: 'Purebasic', value: 'purebasic' },
+    { label: 'Qt Creator Dark', value: 'qtcreatorDark' },
+    { label: 'Qt Creator Light', value: 'qtcreatorLight' },
+    { label: 'Railscasts', value: 'railscasts' },
+    { label: 'Rainbow', value: 'rainbow' },
+    { label: 'RouterOS', value: 'routeros' },
+    { label: 'School Book', value: 'schoolBook' },
+    { label: 'Shades of Purple', value: 'shadesOfPurple' },
     { label: 'Solarized Dark', value: 'solarizedDark' },
     { label: 'Solarized Light', value: 'solarizedLight' },
-    { label: 'Monokai Sublime', value: 'monokaiSublime' },
+    { label: 'Srcery', value: 'srcery' },
+    { label: 'StackOverflow Dark', value: 'stackoverflowDark' },
+    { label: 'StackOverflow Light', value: 'stackoverflowLight' },
+    { label: 'Sunburst', value: 'sunburst' },
+    { label: 'Tomorrow Night Blue', value: 'tomorrowNightBlue' },
+    { label: 'Tomorrow Night Bright', value: 'tomorrowNightBright' },
+    { label: 'Tomorrow Night Eighties', value: 'tomorrowNightEighties' },
     { label: 'Tomorrow Night', value: 'tomorrowNight' },
+    { label: 'Tomorrow', value: 'tomorrow' },
+    { label: 'VS', value: 'vs' },
+    { label: 'VS 2015', value: 'vs2015' },
+    { label: 'Xcode', value: 'xcode' },
+    { label: 'XT 256', value: 'xt256' },
+    { label: 'Zenburn', value: 'zenburn' },
   ]
+
 
   return (
     <React.Fragment>
--- src/components/ContentRenderer/Code.tsx
+++ src/components/ContentRenderer/Code.tsx
@@ -1,35 +1,20 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
-import CodeHighlighter from 'react-native-code-highlighter'
-import {
-  a11yDark,
-  a11yLight,
-  agate,
-  androidstudio,
-  anOldHope,
-  arduinoLight,
-  ascetic,
-  atomOneDark,
-  atomOneLight,
-  dracula,
-  github,
-  monokaiSublime,
-  nightOwl,
-  solarizedDark,
-  solarizedLight,
-  tomorrowNight,
-} from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import CodeHighlighter from 'react-native-code-highlighter';
+import { a11yDark, a11yLight, agate, androidstudio, anOldHope, arduinoLight, arta, ascetic, atelierCaveDark, atelierCaveLight, atelierDuneDark, atelierDuneLight, atelierEstuaryDark, atelierEstuaryLight, atelierForestDark, atelierForestLight, atelierHeathDark, atelierHeathLight, atelierLakesideDark, atelierLakesideLight, atelierPlateauDark, atelierPlateauLight, atelierSavannaDark, atelierSavannaLight, atelierSeasideDark, atelierSeasideLight, atelierSulphurpoolDark, atelierSulphurpoolLight, atomOneDark, atomOneDarkReasonable, atomOneLight, brownPaper, codepenEmbed, colorBrewer, darcula, dark, defaultStyle, docco, dracula, far, foundation, github, githubGist, gml, googlecode, gradientDark, grayscale, gruvboxDark, gruvboxLight, hopscotch, hybrid, idea, irBlack, isblEditorDark, isblEditorLight, kimbieDark, kimbieLight, lightfair, lioshi, magula, monoBlue, monokai, monokaiSublime, nightOwl, nnfx, nnfxDark, nord, obsidian, ocean, paraisoDark, paraisoLight, pojoaque, purebasic, qtcreatorDark, qtcreatorLight, railscasts, rainbow, routeros, schoolBook, shadesOfPurple, solarizedDark, solarizedLight, srcery, stackoverflowDark, stackoverflowLight, sunburst, tomorrow, tomorrowNight, tomorrowNightBlue, tomorrowNightBright, tomorrowNightEighties, vs, vs2015, xcode, xt256, zenburn } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-import useStore from '../../utils/store'
+
+
+import useStore from '../../utils/store';
+
 
 export default function Code({ width, props, value }) {
   const codeStyle = useStore(state => state.hljsStyle)
 
-  
   return (
     <View
       style={{ width: width * 0.93, paddingBottom: 0 }} // można ustawić maksymalną szerokość dla większych ekranów
-      >
+    >
       <CodeHighlighter
         hljsStyle={returnStyle(codeStyle)}
         textStyle={{ fontSize: 16 }}
@@ -55,81 +40,206 @@ const styles = StyleSheet.create({
     elevation: 4,
   },
 })
-
-  function returnStyle(codeStyle) {
-    switch (codeStyle) {
-      case 'nightOwl':
-        return nightOwl
-      case 'ascetic':
-        return ascetic
-      case 'a11yDark':
-        return a11yDark
-      case 'a11yLight':
-        return a11yLight
-      case 'agate':
-        return agate
-      case 'anOldHope':
-        return anOldHope
-      case 'androidstudio':
-        return androidstudio
-      case 'arduinoLight':
-        return arduinoLight
-      case 'atomOneDark':
-        return atomOneDark
-      case 'atomOneLight':
-        return atomOneLight
-      case 'dracula':
-        return dracula
-      case 'github':
-        return github
-      case 'solarizedDark':
-        return solarizedDark
-      case 'solarizedLight':
-        return solarizedLight
-      case 'monokaiSublime':
-        return monokaiSublime
-      case 'tomorrowNight':
-        return tomorrowNight
-      default:
-        return nightOwl
-    }
+function returnStyle(codeStyle) {
+  switch (codeStyle) {
+    case 'a11yDark': return a11yDark;
+    case 'a11yLight': return a11yLight;
+    case 'agate': return agate;
+    case 'anOldHope': return anOldHope;
+    case 'androidstudio': return androidstudio;
+    case 'arduinoLight': return arduinoLight;
+    case 'arta': return arta;
+    case 'ascetic': return ascetic;
+    case 'atelierCaveDark': return atelierCaveDark;
+    case 'atelierCaveLight': return atelierCaveLight;
+    case 'atelierDuneDark': return atelierDuneDark;
+    case 'atelierDuneLight': return atelierDuneLight;
+    case 'atelierEstuaryDark': return atelierEstuaryDark;
+    case 'atelierEstuaryLight': return atelierEstuaryLight;
+    case 'atelierForestDark': return atelierForestDark;
+    case 'atelierForestLight': return atelierForestLight;
+    case 'atelierHeathDark': return atelierHeathDark;
+    case 'atelierHeathLight': return atelierHeathLight;
+    case 'atelierLakesideDark': return atelierLakesideDark;
+    case 'atelierLakesideLight': return atelierLakesideLight;
+    case 'atelierPlateauDark': return atelierPlateauDark;
+    case 'atelierPlateauLight': return atelierPlateauLight;
+    case 'atelierSavannaDark': return atelierSavannaDark;
+    case 'atelierSavannaLight': return atelierSavannaLight;
+    case 'atelierSeasideDark': return atelierSeasideDark;
+    case 'atelierSeasideLight': return atelierSeasideLight;
+    case 'atelierSulphurpoolDark': return atelierSulphurpoolDark;
+    case 'atelierSulphurpoolLight': return atelierSulphurpoolLight;
+    case 'atomOneDarkReasonable': return atomOneDarkReasonable;
+    case 'atomOneDark': return atomOneDark;
+    case 'atomOneLight': return atomOneLight;
+    case 'brownPaper': return brownPaper;
+    case 'codepenEmbed': return codepenEmbed;
+    case 'colorBrewer': return colorBrewer;
+    case 'darcula': return darcula;
+    case 'dark': return dark;
+    case 'defaultStyle': return defaultStyle;
+    case 'docco': return docco;
+    case 'dracula': return dracula;
+    case 'far': return far;
+    case 'foundation': return foundation;
+    case 'githubGist': return githubGist;
+    case 'github': return github;
+    case 'gml': return gml;
+    case 'googlecode': return googlecode;
+    case 'gradientDark': return gradientDark;
+    case 'grayscale': return grayscale;
+    case 'gruvboxDark': return gruvboxDark;
+    case 'gruvboxLight': return gruvboxLight;
+    case 'hopscotch': return hopscotch;
+    case 'hybrid': return hybrid;
+    case 'idea': return idea;
+    case 'irBlack': return irBlack;
+    case 'isblEditorDark': return isblEditorDark;
+    case 'isblEditorLight': return isblEditorLight;
+    case 'kimbieDark': return kimbieDark;
+    case 'kimbieLight': return kimbieLight;
+    case 'lightfair': return lightfair;
+    case 'lioshi': return lioshi;
+    case 'magula': return magula;
+    case 'monoBlue': return monoBlue;
+    case 'monokaiSublime': return monokaiSublime;
+    case 'monokai': return monokai;
+    case 'nightOwl': return nightOwl;
+    case 'nnfxDark': return nnfxDark;
+    case 'nnfx': return nnfx;
+    case 'nord': return nord;
+    case 'obsidian': return obsidian;
+    case 'ocean': return ocean;
+    case 'paraisoDark': return paraisoDark;
+    case 'paraisoLight': return paraisoLight;
+    case 'pojoaque': return pojoaque;
+    case 'purebasic': return purebasic;
+    case 'qtcreatorDark': return qtcreatorDark;
+    case 'qtcreatorLight': return qtcreatorLight;
+    case 'railscasts': return railscasts;
+    case 'rainbow': return rainbow;
+    case 'routeros': return routeros;
+    case 'schoolBook': return schoolBook;
+    case 'shadesOfPurple': return shadesOfPurple;
+    case 'solarizedDark': return solarizedDark;
+    case 'solarizedLight': return solarizedLight;
+    case 'srcery': return srcery;
+    case 'stackoverflowDark': return stackoverflowDark;
+    case 'stackoverflowLight': return stackoverflowLight;
+    case 'sunburst': return sunburst;
+    case 'tomorrowNightBlue': return tomorrowNightBlue;
+    case 'tomorrowNightBright': return tomorrowNightBright;
+    case 'tomorrowNightEighties': return tomorrowNightEighties;
+    case 'tomorrowNight': return tomorrowNight;
+    case 'tomorrow': return tomorrow;
+    case 'vs': return vs;
+    case 'vs2015': return vs2015;
+    case 'xcode': return xcode;
+    case 'xt256': return xt256;
+    case 'zenburn': return zenburn;
+    default: return nightOwl;
   }
+}
 
-  function returnColor(codeStyle) {
-    switch (codeStyle) {
-      case 'nightOwl':
-        return '#011627'
-      case 'ascetic':
-        return 'white'
-      case 'a11yDark':
-        return '#2b2b2b'
-      case 'a11yLight':
-        return '#fefefe'
-      case 'agate':
-        return '#333'
-      case 'anOldHope':
-        return '#1c1d21'
-      case 'androidstudio':
-        return '#282b2e'
-      case 'arduinoLight':
-        return '#ffffff'
-      case 'atomOneDark':
-        return '#282c34'
-      case 'atomOneLight':
-        return '#fafafa'
-      case 'dracula':
-        return '#282a36'
-      case 'github':
-        return '#f8f8f8'
-      case 'solarizedDark':
-        return '#002b36'
-      case 'solarizedLight':
-        return '#fdf6e3'
-      case 'monokaiSublime':
-        return '#23241f'
-      case 'tomorrowNight':
-        return '#1d1f21'
-      default:
-        return '#011627'
-    }
+function returnColor(codeStyle) {
+  switch (codeStyle) {
+    case 'a11yDark': return '#2b2b2b';
+    case 'a11yLight': return '#fefefe';
+    case 'agate': return '#333';
+    case 'anOldHope': return '#1c1d21';
+    case 'androidstudio': return '#282b2e';
+    case 'arduinoLight': return '#ffffff';
+    case 'arta': return '#000000';
+    case 'ascetic': return 'white';
+    case 'atelierCaveDark': return '#19171c';
+    case 'atelierCaveLight': return '#efecf4';
+    case 'atelierDuneDark': return '#20201d';
+    case 'atelierDuneLight': return '#fefbec';
+    case 'atelierEstuaryDark': return '#22221b';
+    case 'atelierEstuaryLight': return '#f4f3ec';
+    case 'atelierForestDark': return '#1b1918';
+    case 'atelierForestLight': return '#f1efee';
+    case 'atelierHeathDark': return '#1b181b';
+    case 'atelierHeathLight': return '#f7f3f7';
+    case 'atelierLakesideDark': return '#161b1d';
+    case 'atelierLakesideLight': return '#ebf8ff';
+    case 'atelierPlateauDark': return '#1b1818';
+    case 'atelierPlateauLight': return '#f4ecec';
+    case 'atelierSavannaDark': return '#171c19';
+    case 'atelierSavannaLight': return '#ecf4ee';
+    case 'atelierSeasideDark': return '#131513';
+    case 'atelierSeasideLight': return '#f4fbf4';
+    case 'atelierSulphurpoolDark': return '#202746';
+    case 'atelierSulphurpoolLight': return '#f5f7ff';
+    case 'atomOneDarkReasonable': return '#282c34';
+    case 'atomOneDark': return '#282c34';
+    case 'atomOneLight': return '#fafafa';
+    case 'brownPaper': return '#d9d9d9';
+    case 'codepenEmbed': return '#222';
+    case 'colorBrewer': return '#ffffff';
+    case 'darcula': return '#2b2b2b';
+    case 'dark': return '#000';
+    case 'defaultStyle': return '#ffffff';
+    case 'docco': return '#f8f8ff';
+    case 'dracula': return '#282a36';
+    case 'far': return '#00070a';
+    case 'foundation': return '#eeeeee';
+    case 'githubGist': return '#ffffff';
+    case 'github': return '#f8f8f8';
+    case 'gml': return '#2d2d2d';
+    case 'googlecode': return '#ffffff';
+    case 'gradientDark': return '#111';
+    case 'grayscale': return '#fefefe';
+    case 'gruvboxDark': return '#282828';
+    case 'gruvboxLight': return '#fdf4c1';
+    case 'hopscotch': return '#322931';
+    case 'hybrid': return '#1d1f21';
+    case 'idea': return '#ffffff';
+    case 'irBlack': return '#000000';
+    case 'isblEditorDark': return '#363c69';
+    case 'isblEditorLight': return '#f3f3f3';
+    case 'kimbieDark': return '#221a0f';
+    case 'kimbieLight': return '#ffffff';
+    case 'lightfair': return '#f0f0f0';
+    case 'lioshi': return '#272822';
+    case 'magula': return '#f4f4f4';
+    case 'monoBlue': return '#eaeef3';
+    case 'monokaiSublime': return '#23241d';
+    case 'monokai': return '#272822';
+    case 'nightOwl': return '#011627';
+    case 'nnfxDark': return '#313550';
+    case 'nnfx': return '#fefefe';
+    case 'nord': return '#2e3440';
+    case 'obsidian': return '#282b2e';
+    case 'ocean': return '#2b303b';
+    case 'paraisoDark': return '#2f1e2e';
+    case 'paraisoLight': return '#e7e9db';
+    case 'pojoaque': return '#ffffff';
+    case 'purebasic': return '#3f3f3f';
+    case 'qtcreatorDark': return '#000000';
+    case 'qtcreatorLight': return '#ffffff';
+    case 'railscasts': return '#2b2b2b';
+    case 'rainbow': return '#474646';
+    case 'routeros': return '#f0f0f0';
+    case 'schoolBook': return '#f9f9f9';
+    case 'shadesOfPurple': return '#2d2b57';
+    case 'solarizedDark': return '#002b36';
+    case 'solarizedLight': return '#fdf6e3';
+    case 'srcery': return '#1c1b19';
+    case 'stackoverflowDark': return '#1c1b19';
+    case 'stackoverflowLight': return '#ffffff';
+    case 'sunburst': return '#000';
+    case 'tomorrowNightBlue': return '#002451';
+    case 'tomorrowNightBright': return '#000';
+    case 'tomorrowNightEighties': return '#2d2d2d';
+    case 'tomorrowNight': return '#1d1f21';
+    case 'tomorrow': return '#ffffff';
+    case 'vs': return '#ffffff';
+    case 'vs2015': return '#1e1e1e';
+    case 'xcode': return '#f8f8f8';
+    case 'xt256': return '#000';
+    case 'zenburn': return '#3f3f3f';
+    default: return '#011627';
   }
+}
