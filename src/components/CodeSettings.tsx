1fd7e0a5479c7f0cb0b20aefe5ec2e099672a98d
Mikołaj <kaleta222@gmail.com>
2024-09-22 09:34:24 +0200

    Squashed commit of the following:
    commit 983a40b8b4a1a3b39ecf96c263e3370470b569b4
    Author: Mikołaj <kaleta222@gmail.com>
    Date:   Sun Sep 22 09:33:01 2024 +0200
    
        feat(Paragraph): set new ins bg color and tilt the letters

--- src/components/CodeSettings.tsx
+++ src/components/CodeSettings.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 
 import { selectCodeTheme } from '../../data/texts'
 import { Colors } from '../utils/constants'
+import { throttle } from '../utils/functions'
 import useStore from '../utils/store'
 import { setValue } from '../utils/utilStorage'
 import ContentRenderer from './ContentRenderer/_ContentRenderer'
@@ -21,103 +22,24 @@ const stylesArray = [
   { label: 'Solarized Dark', value: 'solarizedDark' },
   { label: 'Srcery', value: 'srcery' },
   { label: 'Tomorrow Night Blue', value: 'tomorrowNightBlue' },
-  // { label: 'Atom One Light', value: 'atomOneLight' },
-  // { label: 'Dracula', value: 'dracula' },
-  // { label: 'Hopscotch', value: 'hopscotch' },
-  // { label: 'GitHub', value: 'github' },
-  // { label: 'IR Black', value: 'irBlack' },
-  // { label: 'Nord', value: 'nord' },
-  // { label: 'Tomorrow', value: 'tomorrow' },
-
-  // { label: 'A11y Dark', value: 'a11yDark' },
-  // { label: 'Agate', value: 'agate' },
-  // { label: 'An Old Hope', value: 'anOldHope' },
-  // { label: 'Android Studio', value: 'androidstudio' },
-  // { label: 'Atelier Cave Dark', value: 'atelierCaveDark' },
-  // { label: 'Atelier Estuary Dark', value: 'atelierEstuaryDark' },
-  // { label: 'Atelier Estuary Light', value: 'atelierEstuaryLight' },
-  // { label: 'Atelier Forest Dark', value: 'atelierForestDark' },
-  // { label: 'Atelier Forest Light', value: 'atelierForestLight' },
-  // { label: 'Atelier Heath Dark', value: 'atelierHeathDark' },
-  // { label: 'Atelier Heath Light', value: 'atelierHeathLight' },
-  // { label: 'Atelier Plateau Dark', value: 'atelierPlateauDark' },
-  // { label: 'Atelier Plateau Light', value: 'atelierPlateauLight' },
-  // { label: 'Atelier Seaside Dark', value: 'atelierSeasideDark' },
-  // { label: 'Atelier Sulphurpool Dark', value: 'atelierSulphurpoolDark' },
-  // { label: 'Atelier Sulphurpool Light', value: 'atelierSulphurpoolLight' },
-  // { label: 'Atom One Dark Reasonable', value: 'atomOneDarkReasonable' },
-  // { label: 'Darcula', value: 'darcula' },
-  // { label: 'Codepen Embed', value: 'codepenEmbed' },
-  // { label: 'Foundation', value: 'foundation' },
-  // { label: 'GitHub Gist', value: 'githubGist' },
-  // { label: 'Google Code', value: 'googlecode' },
-  // { label: 'Hybrid', value: 'hybrid' },
-  // { label: 'Mono Blue', value: 'monoBlue' },
-  // { label: 'Monokai Sublime', value: 'monokaiSublime' },
-  // { label: 'NNFX', value: 'nnfx' },
-  // { label: 'Obsidian', value: 'obsidian' },
-  // { label: 'Ocean', value: 'ocean' },
-  // { label: 'Qt Creator Dark', value: 'qtcreatorDark' },
-  // { label: 'Qt Creator Light', value: 'qtcreatorLight' },
-  // { label: 'RouterOS', value: 'routeros' },
-  // { label: 'StackOverflow Dark', value: 'stackoverflowDark' },
-  // { label: 'Sunburst', value: 'sunburst' },
-  // { label: 'Tomorrow Night Bright', value: 'tomorrowNightBright' },
-  // { label: 'Tomorrow Night', value: 'tomorrowNight' },
-  // { label: 'VS', value: 'vs' },
-  // { label: 'Arduino Light', value: 'arduinoLight' },
-  // { label: 'Arta', value: 'arta' },
-  // { label: 'Ascetic', value: 'ascetic' },
-  // { label: 'Atelier Dune Dark', value: 'atelierDuneDark' },
-  // { label: 'Atelier Dune Light', value: 'atelierDuneLight' },
-  // { label: 'Atelier Lakeside Dark', value: 'atelierLakesideDark' },
-  // { label: 'Atelier Lakeside Light', value: 'atelierLakesideLight' },
-  // { label: 'Atelier Savanna Dark', value: 'atelierSavannaDark' },
-  // { label: 'Atelier Savanna Light', value: 'atelierSavannaLight' },
-  // { label: 'Atelier Seaside Light', value: 'atelierSeasideLight' },
-  // { label: 'Brown Paper', value: 'brownPaper' },
-  // { label: 'Color Brewer', value: 'colorBrewer' },
-  // { label: 'Dark', value: 'dark' },
-  // { label: 'Default Style', value: 'defaultStyle' },
-  // { label: 'Far', value: 'far' },
-  // { label: 'GML', value: 'gml' },
-  // { label: 'Gradient Dark', value: 'gradientDark' },
-  // { label: 'Grayscale', value: 'grayscale' },
-  // { label: 'Gruvbox Light', value: 'gruvboxLight' },
-  // { label: 'Idea', value: 'idea' },
-  // { label: 'ISBL Editor Dark', value: 'isblEditorDark' },
-  // { label: 'ISBL Editor Light', value: 'isblEditorLight' },
-  // { label: 'Kimbie Light', value: 'kimbieLight' },
-  // { label: 'Lightfair', value: 'lightfair' },
-  // { label: 'Lioshi', value: 'lioshi' },
-  // { label: 'Magula', value: 'magula' },
-  // { label: 'Paraiso Light', value: 'paraisoLight' },
-  // { label: 'NNFX Dark', value: 'nnfxDark' },
-  // { label: 'Pojoaque', value: 'pojoaque' },
-  // { label: 'Purebasic', value: 'purebasic' },
-  // { label: 'Railscasts', value: 'railscasts' },
-  // { label: 'Rainbow', value: 'rainbow' },
-  // { label: 'School Book', value: 'schoolBook' },
-  // { label: 'Solarized Light', value: 'solarizedLight' },
-  // { label: 'StackOverflow Light', value: 'stackoverflowLight' },
-
-  // { label: 'Tomorrow Night Eighties', value: 'tomorrowNightEighties' },
-  // { label: 'VS 2015', value: 'vs2015' },
-  // { label: 'Xcode', value: 'xcode' },
-  // { label: 'XT 256', value: 'xt256' },
-  // { label: 'Zenburn', value: 'zenburn' },
 ]
 
 export default function CodeSettings() {
   const setHljsStyle = useStore(state => state.setHljsStyle)
   const hljsStyle = useStore(state => state.hljsStyle)
 
-  const [localStyle, setLocalStyle] = useState(hljsStyle) // helper variable improving performance
+  const [localStyle, setLocalStyle] = useState(hljsStyle)
 
   useEffect(() => {
     setHljsStyle(localStyle)
-    setValue('hljsStyle', hljsStyle)
+    setValue('hljsStyle', localStyle)
   }, [localStyle])
+
+  // Funkcja opakowana w throttle z limitem 500ms
+  const handlePressThrottled = useCallback(
+    throttle(value => setLocalStyle(value), 500),
+    [],
+  )
 
   return (
     <React.Fragment>
@@ -136,7 +58,6 @@ export default function CodeSettings() {
           {selectCodeTheme}
         </Text>
       </View>
-      {/* <LinearGradient style={{}}></LinearGradient> */}
       <ScrollView contentContainerStyle={{ paddingBottom: 30 }}>
         <View style={styles.optionsContainer}>
           {stylesArray.map(item => (
@@ -147,7 +68,7 @@ export default function CodeSettings() {
                 styles.optionButton,
                 localStyle === item.value && styles.selectedOption,
               ]}
-              onPress={() => setLocalStyle(item.value)}
+              onPress={() => handlePressThrottled(item.value)} // użycie throttled function
             >
               <Text
                 style={[
