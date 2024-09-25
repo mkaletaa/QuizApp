2852df5334bbb6e503bd4104507494d7bbbeaf9e
Mikołaj <kaleta222@gmail.com>
2024-09-25 13:00:18 +0200

    data(asynchronicity): finish event_loop quiz

--- data/quiz/asynchronicity/asynchronicity.ts
+++ data/quiz/asynchronicity/asynchronicity.ts
@@ -1,4 +1,5 @@
 import async_and_await from './async_and_await.json'
+import event_loop from './event_loop.json'
 import promises from './promises.json'
 import timeouts_and_intervals from './timeouts_and_intervals.json'
 
@@ -6,4 +7,5 @@ export const asynchronicity = {
   timeouts_and_intervals,
   promises,
   async_and_await,
+  event_loop,
 }
