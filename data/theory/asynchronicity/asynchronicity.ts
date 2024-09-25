705f82c2b1bc1183e30cac3aaa47753c1d1e8d99
Mikołaj <kaleta222@gmail.com>
2024-09-25 11:29:10 +0200

    data(asynchronicity): finish event_loop theory

--- data/theory/asynchronicity/asynchronicity.ts
+++ data/theory/asynchronicity/asynchronicity.ts
@@ -1,9 +1,11 @@
-import promises from './promises.json'
 import async_and_await from './async_and_await.json'
+import event_loop from './event_loop.json'
+import promises from './promises.json'
 import timeouts_and_intervals from './timeouts_and_intervals.json'
 
 export const asynchronicity = {
-    timeouts_and_intervals,
-    promises,
-    async_and_await,
+  timeouts_and_intervals,
+  promises,
+  async_and_await,
+  event_loop,
 }
