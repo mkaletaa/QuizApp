83fc05dcc4898264036d83a48b754d7787d75ee8
Mikołaj <kaleta222@gmail.com>
2024-09-26 10:12:44 +0200

    data(OOP): finish inbuilt objects

--- data/theory/Object_oriented_JS/Object_oriented_JS.ts
+++ data/theory/Object_oriented_JS/Object_oriented_JS.ts
@@ -1,7 +1,9 @@
 import custom_types from './custom_types.json'
+import inbuilt_objects from './inbuilt_objects.json'
 import object_methods from './object_methods.json'
 
 export const Object_oriented_JS = {
   object_methods,
   custom_types,
+  inbuilt_objects,
 }
